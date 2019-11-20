e415006f361c4aef8c64960157d4b9fb3bff3b31
GuyCalloway <guyfcalloway@gmail.com>
2019-11-20 15:50:08 +0000

    Three methods plotted on same graph using matlibplot

--- methods.py
+++ methods.py
@@ -1,2 +1,28 @@
-def get_first(data):
-    return data[0]
+import math
+
+
+class Methods:
+    def get_first(self, data):
+        return data[0]
+
+    def count(self, data):
+        counter = 0
+        for i in data:
+            if i == 5:
+                counter += 1
+        return counter
+
+    def no_primes(self, data):
+        def is_prime(x):
+            if x >= 2:
+                for y in range(2, int(math.sqrt(x))):
+                    if not (x % y):
+                        return False
+            else:
+                return False
+            return True
+        counter = 0
+        for i in data:
+            if is_prime(i):
+                counter += 1
+        return counter
--- methods_test.py
+++ methods_test.py
@@ -1,9 +1,9 @@
 import unittest
 import numpy as np
-from methods import get_first
+from methods import Methods
 import time
 import matplotlib.pyplot as plt
-#import timeit
+# import timeit
 
 
 class Timer:
@@ -12,25 +12,32 @@ class Timer:
         self.speeds = []
         self.arraySize = []
 
-    def array_builder(self, number):
+    def array_builder(self, upperlimit, number):
         self.arraySize.append(number)
-        return np.random.randint(1, 101, number)
+        return np.random.randint(1, upperlimit, number)
 
-    def run_test(self, array, method):
+    def run_timer(self, array, method):
         start = time.process_time()
         method(array)
         end = time.process_time()
-        speed = (end - start)*1000
+        speed = (end - start)
         self.speeds.append(speed)
 
 
-if __name__ == '__main__':
+def test_and_graph(range1, increments, upperlimit, method):
     timeTest = Timer()
-    for i in range(1, 1000000, 5000):
-        test_list = timeTest.array_builder(i)
+    for i in range(1, range1, increments):
+        test_list = timeTest.array_builder(upperlimit, i)
         # get_first is just method taking index 0 of array
-        timeTest.run_test(test_list, get_first)
+        timeTest.run_timer(test_list, method)
     plt.plot(timeTest.arraySize, timeTest.speeds)
-    plt.ylabel('execution Time(seconds)')
+
+
+if __name__ == '__main__':
+    methods = Methods()
+    test_and_graph(10000, 50, 150, methods.get_first)  # blue
+    test_and_graph(10000, 50, 150, methods.count)  # orange
+    test_and_graph(10000, 50, 150, methods.no_primes)  # green
+    plt.ylabel('execution Time(ms)')
     plt.xlabel('number of items in List')
     plt.show()
