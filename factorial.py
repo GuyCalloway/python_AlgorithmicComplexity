0984a2746d9a5d0aa3167cd2a090dc32cc3d8c9d
GuyCalloway <guyfcalloway@gmail.com>
2020-01-31 17:25:57 +0000

    Added recursive and iterative Factorial tools

---
+++ factorial.py
@@ -0,0 +1,21 @@
+import timeit
+import functools
+
+
+def factorial(int):
+    tot = 1
+    for num in range(int):
+        tot *= (int - num)
+    return tot
+
+
+def factorial2(int):
+    if int == 1:
+        return 1
+    else:
+        return int * factorial2(int-1)
+
+
+for f in [factorial, factorial2]:
+    print('{} takes {} usec/loop'.format(f.__name__,
+                                         timeit.timeit(functools.partial(f, 10))))
